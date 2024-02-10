695d09f3079117b8d6cf4bd071e7a3539596ee47
Rinochki <fenixrina212@gmail.com>
2024-02-10 22:09:45 +0200

    Прописала функції
    головну, введення та 2 перевірки

--- pythonProject12/main.py
+++ pythonProject12/main.py
@@ -1,17 +1,29 @@
 def main():
-    pass
+    print("Доброго дня. Почнемо введення даних.")
+    data = get_user_input()
+    year_true = validate_month(mounth=data[1])
 
 
-def validate_month():
-    pass
+def validate_month(mounth):
+    mounths = ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"]
+    if mounth in mounths:
+        return True
+    else:
+        return False
 
 
-def validate_year():
-    pass
+def validate_year(year):
+    if year < 1904 or year > 2019:
+        return False
+    else:
+        return True
 
 
 def get_user_input():
-    pass
+    name = input("введіть своє прізвище та ім'я> ")
+    mounth = input("введіть місяць в якому ви народилися> ")
+    year = input(int("введіть рік свого народження> "))
+    return [name, mounth, year]
 
 
 def create_test_file():
